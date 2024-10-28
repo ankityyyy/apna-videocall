b3fe92c9153c5f5d8a47d79b11d39e090d5276c2
ankit dubey <ankitdubey9431969639@gmail.com>
2024-10-28 17:13:38 +0530

    add frontend  origin

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-// const PORT = process.env.PORT || 3002;
-// const uri = process.env.ATLASDB_URL;
-
-// import express from "express";
-// import { createServer } from "node:http";
-// import { Server } from "socket.io";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import { connectToSocket } from "./src/controllers/socketManager.js";
-// import userRoutes from "./src/routes/user.js";
-
-// const app = express();
-// const server = createServer(app); // Create HTTP server with Express app
-// const io = connectToSocket(server);
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.json({ limit: "40kb" }));
-// app.use(express.urlencoded({ limit: "40kb", extended: true }));
-
-// app.use("/api/v1/users", userRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("hello");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`listen on port no  ${PORT}`);
-//   mongoose.connect(uri);
-//   console.log("data base is connect");
-// });
-
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 3002;
@@ -51,7 +17,10 @@ const server = createServer(app); // Create HTTP server with Express app
 // Set up CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Update to your client URL
+    origin: [
+      "http://localhost:5173",
+      "https://apna-videocall-frontend.onrender.com",
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["*"],
     credentials: true,
--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -7,7 +7,10 @@ let timeOnline = {};
 export const connectToSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173", // Allow your client app origin
+      origin: [
+        "http://localhost:5173",
+        "https://apna-videocall-frontend.onrender.com",
+      ],
       methods: ["GET", "POST"],
       allowedHeaders: ["*"],
       credentials: true,
